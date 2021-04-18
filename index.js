a68eb1669ffabade52d36ef92d1ae605ce5036ca
Ben Echols <benjamin.echols@gmail.com>
2021-04-17 17:31:27 -0700

    CI works! Re-adding gifs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ exports.handler = async function (event) {
       "No auth token found, please set the AUTH_TOKEN environment variable.\n"
     );
     process.exit();
-  }  
+  }
 
   // Verify the request. Need to do this or Discord will disable the integration.
   const signature = event.headers["x-signature-ed25519"];
@@ -23,7 +23,7 @@ exports.handler = async function (event) {
     signature,
     timestamp,
     CLIENT_PUBLIC_KEY
-  );  
+  );
   if (!isValidRequest) {
     console.log("Bad request");
     return {
@@ -33,26 +33,26 @@ exports.handler = async function (event) {
       }),
     };
   }
-  
+
   const body = JSON.parse(event.body);
 
   // It's an invocation of the slash command
   if (body && body.type === InteractionType.APPLICATION_COMMAND) {
     const gifs = [
-      //"https://i.gifer.com/9I2.gif",
-      //"https://media.giphy.com/media/nJPkKr231dvKo/giphy.gif",
-      //"https://i.gifer.com/2AZI.gif",
-      //"https://i.imgur.com/6DpDNFS.gif",
-      "http://gph.is/1CF6Lgl", // Duh we're in the Rockies
+      "https://i.gifer.com/9I2.gif", // Ketchup
+      "https://media.giphy.com/media/nJPkKr231dvKo/giphy.gif", // High five
+      "https://i.gifer.com/2AZI.gif", // Limo
+      "https://i.imgur.com/6DpDNFS.gif", // Tuxedo
+      "http://gph.is/1CF6Lgl", // We're in the Rockies
     ];
-  
+
     return {
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
       data: {
         content: `${gifs[Math.floor(Math.random() * gifs.length)]}`,
       },
     };
-  // Or it's just Discord pinging the endpoint, which we need to ack for the integration to keep working
+    // Or it's just Discord pinging the endpoint, which we need to ack for the integration to keep working
   } else {
     console.log("responding with PONG");
     return {
